b5de5098c83ba87d47f079591ecbf2618e0a1215
Matthew Westphall <westphallm1@protonmail.com>
2026-01-19 22:13:06 -0600

    add routine for connecting disparate zones via tunnels

--- Content/Subworlds/ConnectLargeZonesGenPass.cs
+++ Content/Subworlds/ConnectLargeZonesGenPass.cs
@@ -1,12 +1,75 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.GameContent.Generation;
 using Terraria.ID;
 using Terraria.IO;
 using Terraria.ModLoader;
 using Terraria.WorldBuilding;
 
 namespace TerraTrial.Content.Subworlds;
+
+static class PointExtensions
+{
+    internal static int DistanceSquared(this Point p1, Point p2)
+    {
+        return (p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y);
+    }
+
+    internal static float Distance(this Point p1, Point p2)
+    {
+        return MathF.Sqrt(p1.DistanceSquared(p2));
+    }    
+}
+
+internal class RectangleToPoint(Point endPoint, int baseWidth) : GenShape
+{
+    public override bool Perform(Point origin, GenAction action)
+    {
+        var dx = endPoint.X - origin.X;
+        var dy = endPoint.Y - origin.Y;
+
+        var stepVector = new Vector2(dx, dy).SafeNormalize(default) / 2f;
+        var tangentVector = stepVector.RotatedBy(MathF.PI / 2);
+        var steps = (int)origin.Distance(endPoint);
+        var startPos = new Vector2(origin.X, origin.Y);
+
+        for (var i = -2; i < steps * 2 + 2; i++)
+        {
+            for(var j = -baseWidth; j < baseWidth; j++)
+            {
+                var currentPos = startPos + i * stepVector + j * tangentVector;
+                var currentPoint = new Point((int)currentPos.X, (int)currentPos.Y);
+                if (currentPoint.X < 0 || currentPoint.Y < 0 || currentPoint.X >= Main.maxTilesX ||
+                    currentPoint.Y >= Main.maxTilesY)
+                {
+                    continue;
+                }
+                if (!UnitApply(action, origin, currentPoint.X, currentPoint.Y) && _quitOnFail)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+internal class ClearNonimportantTiles : GenAction
+{
+    public override bool Apply(Point origin, int x, int y, params object[] args)
+    {
+        var tile = Main.tile[x, y];
+        if (!Main.tileFrameImportant[tile.TileType])
+        {
+            WorldUtils.ClearTile(x, y, true);
+        }
+        return true;
+    }
+}
 
 internal class WorldZonesModSystem() : ModSystem
 {
@@ -20,7 +83,7 @@ internal class WorldZonesModSystem() : ModSystem
     // Down-sampled 2D array of tiles in the world, true if tile is not passable by player
     public bool[,] SolidsMask { get; private set;  }
     
-    private readonly (int, int)[] _cardinals = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+    private readonly Point[] _cardinals = [new(-1, 0), new(1, 0), new(0, -1), new(0, 1)];
     private readonly ushort[] _wallIds =
     [
         WallID.RubyGemspark,
@@ -29,7 +92,6 @@ internal class WorldZonesModSystem() : ModSystem
         WallID.AmethystGemspark,
         WallID.TopazGemspark,
         WallID.AmberGemspark,
-        WallID.DiamondGemspark
     ];
 
     
@@ -59,25 +121,25 @@ internal class WorldZonesModSystem() : ModSystem
     /// </summary>
     /// <param name="startPoint"></param>
     /// <param name="label"></param>
-    private void LabelZone((int, int) startPoint, int label)
+    private void LabelZone(Point startPoint, int label)
     {
-        var fillQueue = new Stack<(int, int)>();
+        var fillQueue = new Stack<Point>();
         fillQueue.Push(startPoint);
-        Zones[startPoint.Item1, startPoint.Item2] = label;
+        Zones[startPoint.X, startPoint.Y] = label;
 
         while (fillQueue.Count > 0)
         {
             var current = fillQueue.Pop();
             foreach (var (c1, c2) in _cardinals)
             {
-                var x = current.Item1 + c1;
-                var y = current.Item2 + c2;
+                var x = current.X + c1;
+                var y = current.Y + c2;
                 if (x <= 0 || y < 0 || x >= Zones.GetLength(0) || y >= Zones.GetLength(1))
                 {
                     continue;
                 }
                 if (SolidsMask[x, y] || Zones[x, y] != 0) continue;
-                fillQueue.Push((x, y));
+                fillQueue.Push(new Point(x, y));
                 Zones[x, y] = label;
             }
         }
@@ -122,13 +184,13 @@ internal class WorldZonesModSystem() : ModSystem
         }
     }
 
-    private IEnumerable<(int, int)> IterateZones()
+    private IEnumerable<Point> IterateZones()
     {
         for (var i = 0; i < Zones.GetLength(0); i++)
         {
             for (var j = 0; j < Zones.GetLength(1); j++)
             {
-                yield return (i, j);
+                yield return new Point(i, j);
             }
         }
     }
@@ -156,49 +218,88 @@ internal class WorldZonesModSystem() : ModSystem
         return false;
     }
     
-    private Dictionary<int, List<(int, int)>> GenerateEdgeLists(List<int> rankedZones, int zonesToShow)
+    private Dictionary<int, List<Point>> GenerateEdgeLists(List<int> rankedZones, int zonesToShow)
     {
         var zonesToConsider = rankedZones.Take(zonesToShow).ToList();
-        var edgeLists = new Dictionary<int, List<(int, int)>>();
+        var edgeLists = new Dictionary<int, List<Point>>();
         foreach (var (i, j) in IterateZones())
         {
             var label = Zones[i, j];
             if (!zonesToConsider.Contains(label) || !IsEdgeTile(i, j)) continue;
             if (edgeLists.TryGetValue(label, out var edgeList))
             {
-                edgeList.Add((i, j));
+                edgeList.Add(new Point(i, j));
             }
             else
             {
-                edgeLists[label] = [(i, j)];
+                edgeLists[label] = [new Point(i, j)];
             }
         }
 
         return edgeLists;
     }
 
-    private void DebugAddWallsToZones(List<int> rankedZones, int zonesToShow)
+    private bool TryFindZoneNearpoints(List<Point> edge1, List<Point> edge2, out Point minP1, out Point minP2, int maxDistSq = 100*100)
     {
-        foreach (var (i, j) in IterateZones())
+        minP1 = default;
+        minP2 = default;
+        var minDistSq = int.MaxValue;
+        foreach (var p1 in edge1)
         {
-            for (var w = 0; w < zonesToShow; w++)
+            foreach (var p2 in edge2)
             {
-                if (Zones[i, j] != rankedZones[w]) continue;
-                    
-                for (var i2 = 0; i2 < XDownSample; i2++)
+                var dist = p1.DistanceSquared(p2);
+                if (dist < minDistSq && dist < maxDistSq)
                 {
-                    for (var j2 = 0; j2 < YDownSample; j2++)
-                    {
-                        var tile = Main.tile[i * XDownSample + i2, j * YDownSample + j2];
-                        tile.WallType = _wallIds[w % _wallIds.Length];
-                    }
+                    minDistSq = dist;
+                    minP1 = p1;
+                    minP2 = p2;
                 }
-                break;
             }
         }
+        return minP1 != default && minP2 != default;
     }
     
-    private void DebugAddWallsToEdges(Dictionary<int, List<(int, int)>> edges)
+    private void ClearTilesInDiagonal(Point startPoint, Point endPoint) {}
+    /// <summary>
+    /// Given a list of the edges of the contiguous zones of the map, find the
+    /// points of other contiguous zones which come the closest to intersecting
+    /// </summary>
+    /// <param name="edgeLists"></param>
+    private List<(Point, Point)> FindAllZoneNearpoints(Dictionary<int, List<Point>> edgeLists, int maxDist = 100)
+    {
+        var nearPoints = new List<(Point, Point)>();
+        var edgesList = edgeLists.Values.ToList();
+        for (var i = 0; i < edgesList.Count; i++)
+        {
+            for (var j = i + 1; j < edgesList.Count; j++)
+            {
+                var edge1 = edgesList[i];
+                var edge2 = edgesList[j];
+                if (edge1 != edge2 && TryFindZoneNearpoints(edge1, edge2, out var p1, out var p2, maxDist * maxDist))
+                {
+                    nearPoints.Add((p1, p2));
+                }
+            }
+        }
+        return nearPoints;
+    }
+    
+    private void ClearTilesBetweenNearpoints(List<(Point, Point)> nearPoints) 
+    {
+        foreach (var (start, end) in nearPoints)
+        {
+            var worldCoordsStart = new Point(start.X * XDownSample, start.Y * YDownSample);
+            var worldCoordsEnd = new Point(end.X * XDownSample, end.Y * YDownSample);
+            WorldUtils.Gen(
+                worldCoordsStart, 
+                new RectangleToPoint(worldCoordsEnd, 4), 
+                new ClearNonimportantTiles());
+        }
+        
+    }
+
+    private void DebugAddWallsToEdges(Dictionary<int, List<Point>> edges)
     {
         foreach (var (idx, edgeList) in edges)
         {
@@ -215,6 +316,25 @@ internal class WorldZonesModSystem() : ModSystem
             } 
         }
     }
+    
+    private void DebugAddWallsToZoneNearPoints(List<(Point, Point)> nearPoints)
+    {
+        foreach (var ((x1, y1), (x2, y2)) in nearPoints)
+        {
+            for (var i = 0; i < XDownSample; i++)
+            {
+                for (var j = 0; j < YDownSample; j++)
+                {
+                    var tile = Main.tile[x1 * XDownSample + i, y1 * YDownSample + j];
+                    tile.WallType = WallID.DiamondGemspark;
+                    tile = Main.tile[x2 * XDownSample + i, y2 * YDownSample + j];
+                    tile.WallType = WallID.DiamondGemspark;
+                }
+            }
+            
+        }
+        
+    }
 
     internal void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
     {
@@ -230,27 +350,24 @@ internal class WorldZonesModSystem() : ModSystem
             var x = chest.x / XDownSample;
             var y = chest.y / YDownSample;
             if (Zones[x, y] != 0 || SolidsMask[x, y]) continue;
-            LabelZone((x, y), zoneLabel);
+            LabelZone(new Point(x, y), zoneLabel);
             zoneLabel++;
         }
         Mod.Logger.Info($"Generated zones for {zoneLabel} chests");
         
-        /*
-        foreach (var (i, j) in IterateZones())
-        {
-            if (Zones[i, j] != 0 || SolidsMask[i, j]) continue;
-            LabelZone((i, j), zoneLabel);
-            zoneLabel++;
-        }
-        */
         var rankedZones = RankZoneSizes(zoneLabel);
         
         // DebugAddWallsToZones(rankedZones, zoneLabel - 2);
 
         var edges = GenerateEdgeLists(rankedZones, zoneLabel - 2);
+
+        var nearPoints = FindAllZoneNearpoints(edges, 30);
+        
+        ClearTilesBetweenNearpoints(nearPoints);
         
         DebugAddWallsToEdges(edges);
-
+        
+        DebugAddWallsToZoneNearPoints(nearPoints);
     }
 
     
